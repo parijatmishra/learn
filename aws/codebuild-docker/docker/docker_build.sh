a6f4070b43b5da6d554cc157568e991939b1d011
Parijat Mishra <parijat.mishra@gmail.com>
2017-11-12 00:22:09 +0800

    docker/docker_build.sh use 'set -evx' to add automatic debug output to script; remove echo commands which are no longer needed

--- aws/codebuild-docker/docker/docker_build.sh
+++ aws/codebuild-docker/docker/docker_build.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
+set -evx
 VARS="$(dirname $0)/_vars.sh"
 if [ ! -f "${VARS}" ]; then
-    echo  "I need a \"${VARS}\" to run"
     exit 1
 fi
 source "${VARS}"
 [ -z "$IMAGE_REPO_NAME" ] && echo "Environment variable IMAGE_REPO_NAME is not set" && exit 1
 [ -z "$IMAGE_TAG" ] && echo "Environment variable IMAGE_TAG is not set" && exit 1
 
-echo "Building image ${IMAGE_REPO_NAME}:${IMAGE_TAG} ..."
 TMPDIR="docker_build_$$"
 mkdir -p $TMPDIR
 cp $(dirname $0)/app.jar $TMPDIR/app.jar
@@ -17,4 +16,3 @@ docker build --force-rm \
     -t ${IMAGE_REPO_NAME}:${IMAGE_TAG} \
     $TMPDIR/
 rm -rf $TMPDIR
-echo "Done"
