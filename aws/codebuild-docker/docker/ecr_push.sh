798796be4cb3a7454a03ea8c1ef3d13fb78a4b37
Parijat Mishra <parijat.mishra@gmail.com>
2017-11-12 00:22:42 +0800

    docker/ecr_build.sh: detect docker CLI version and use appropriate 'ecr login' flags; use 'set -evx' to add automatic debug output to script; remove echo commands which are no longer needed

--- aws/codebuild-docker/docker/ecr_push.sh
+++ aws/codebuild-docker/docker/ecr_push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
+set -evx
 VARS="$(dirname $0)/_vars.sh"
 if [ ! -f "${VARS}" ]; then
-    echo  "I need a \"${VARS}\" to run"
     exit 1
 fi
 source "${VARS}"
@@ -10,8 +10,14 @@ source "${VARS}"
 [ -z "$AWS_DEFAULT_REGION" ] && (echo "Environment variable AWS_DEFAULT_REGION is not set" && exit 1)
 [ -z "$AWS_ACCOUNT_ID" ] && (echo "Environment variable AWS_ACCOUNT_ID is not set" && exit 2)
 
-$(aws ecr get-login --no-include-email --region ${AWS_DEFAULT_REGION})
-echo "Tagging and pushing ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG} ..."
-docker tag ${IMAGE_REPO_NAME}:${IMAGE_TAG} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG}
-docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG}
-echo "Done"
+DOCKER_MAJOR_VERSION=$(docker --version | cut -f 3 -d ' ' | cut -f 1 -d '.')
+if [ ${DOCKER_MAJOR_VERSION} -ge 17 ]; then
+    # Create and execute CLI for docker version 17.x and higher
+    $(aws ecr get-login --no-include-email --region ${AWS_DEFAULT_REGION})
+else
+    # Create and execute CLI for docker version 1.12.x
+    # Note: we do not explicitly check the version -- we are blindly assuming that the version is 1.12.x
+    $(aws ecr get-login --region ${AWS_DEFAULT_REGION})
+fi
+docker tag "${IMAGE_REPO_NAME}:${IMAGE_TAG}" "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG}"
+docker push "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG}"
