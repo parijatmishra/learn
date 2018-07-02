722eac8576b93c877972a44f73ced165b5f1d0f5
Parijat Mishra <parijat.mishra@gmail.com>
2018-07-02 12:40:58 +0700

    Working React+AppSync+DDB demo app

--- aws/appsync/react/src/App.js
+++ aws/appsync/react/src/App.js
@@ -1,56 +1,123 @@
+import AWSAppSyncClient from "aws-appsync";
+import { Rehydrated } from "aws-appsync-react";
+import { AUTH_TYPE } from "aws-appsync/lib/link/auth-link";
+import { graphql, ApolloProvider, compose } from "react-apollo";
+import * as AWS from "aws-sdk";
+import AppSync from './AppSync.js';
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import AllPosts from "./Components/AllPosts";
 import AddPost from "./Components/AddPost";
+import AllPostsQuery from './Queries/AllPostsQuery';
+import NewPostMutation from './Queries/NewPostMutation';
+import DeletePostMutation from './Queries/DeletePostMutation';
+import UpdatePostMutation from './Queries/UpdatePostMutation';
 
-const posts = [{ "id": "1", "title": "My first post", "author": "Someone" }, { "id": "2", "title": "Another post", "author": "Someone else" }, { "id": "3", "title": "A better one", "author": "Stranger" }];
+const client = new AWSAppSyncClient({
+    url: AppSync.graphqlEndpoint,
+    region: AppSync.region,
+    auth: {
+        type: AUTH_TYPE.API_KEY,
+        apiKey: AppSync.apiKey,
+    },
+    disableOffline: true
+});
 
 class App extends Component {
-    state = { posts };
-
-    handleOnAdd = (post) => {
-        const { posts } = this.state;
-
-        this.setState({
-            posts: [...posts, post]
-        });
-    };
-
-    handleOnDelete = ({id}) => {
-        const { posts } = this.state;
-
-        this.setState({
-            posts: [...posts.filter(post => post.id !== id)]
-        });
-    }
-
-    handleOnEdit = (editedPost) => {
-        const { posts } = this.state;
-
-        this.setState({
-            posts: [...posts.map(post => post.id === editedPost.id ? editedPost : post)]
-        });
-    }
-
     render() {
-        const { posts } = this.state;
-
         return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to React</h1>
+                <h1 className="App-title">Welcome to React-AppSync-DynamoDB Demo</h1>
             </header>
-            <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-            <AddPost onAdd={this.handleOnAdd} />
-            <AllPosts posts={posts} onDelete={this.handleOnDelete} onEdit={this.handleOnEdit} />
+            <NewPostWithData />
+            <AllPostsWithData />
         </div>
         );
     }
 }
 
-export default App;
+const AllPostsWithData = compose(
+graphql(AllPostsQuery, {
+    options: {
+        fetchPolicy: 'cache-and-network'
+    },
+    props: (props) => ({
+        posts: props.data.allPost && props.data.allPost.posts,
+    })
+}),
+graphql(DeletePostMutation, {
+    props: (props) => ({
+        onDelete: (post) => props.mutate({
+            variables: { id: post.id, expectedVersion: post.version },
+            optimisticResponse: () => ({ deletePost: { ...post, __typename: 'Post' } }),
+        })
+    }),
+    options: {
+        refetchQueries: [{ query: AllPostsQuery }],
+        update: (proxy, { data: { deletePost: { id } } }) => {
+            const query = AllPostsQuery;
+            const data = proxy.readQuery({ query });
+
+            data.allPost.posts = data.allPost.posts.filter(post => post.id !== id);
+
+            proxy.writeQuery({ query, data });
+        }
+    }
+}),
+graphql(UpdatePostMutation, {
+    props: (props) => ({
+        onEdit: (post) => {
+            props.mutate({
+            variables: { ...post, expectedVersion: post.version },
+            optimisticResponse: () => ({ updatePost: { ...post, __typename: 'Post', version: post.version + 1 } }),
+            })
+        }
+    }),
+    options: {
+        refetchQueries: [{ query: AllPostsQuery }],
+        update: (dataProxy, { data: { updatePost } }) => {
+            const query = AllPostsQuery;
+            const data = dataProxy.readQuery({ query });
+
+            data.allPost.posts = data.allPost.posts.map(post => post.id !== updatePost.id ? post : { ...updatePost });
+
+            dataProxy.writeQuery({ query, data });
+        }
+    }
+})
+)(AllPosts);
+
+const NewPostWithData = graphql(NewPostMutation, {
+    props: (props) => ({
+        onAdd: post => props.mutate({
+            variables: post,
+            optimisticResponse: () => ({ addPost: { ...post, __typename: 'Post', version: 1 } }),
+        })
+    }),
+    options: {
+        refetchQueries: [{ query: AllPostsQuery }],
+        update: (dataProxy, { data: { addPost } }) => {
+            const query = AllPostsQuery;
+            const data = dataProxy.readQuery({ query });
+
+            data.allPost.posts.push(addPost);
+
+            dataProxy.writeQuery({ query, data });
+        }
+    }
+})(AddPost);
+
+const WithProvider = () => (
+    <ApolloProvider client={client}>
+        <Rehydrated>
+            <App />
+        </Rehydrated>
+    </ApolloProvider>
+);
+
+export default WithProvider;
---
+++ aws/appsync/react/src/AppSync.js
@@ -0,0 +1,6 @@
+﻿export default {
+    "graphqlEndpoint": "https://d6ab4tyanzctziq57dmblnwg7u.appsync-api.us-east-1.amazonaws.com/graphql",
+    "region": "us-east-1",
+    "authenticationType": "API_KEY",
+    "apiKey": "da2-xqos4gfoi5dz5fjo7kyqvb4wga"
+}
